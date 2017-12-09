5076e56f920f24981c434b68c48c4aed1c366440
ChrisBAshton <chrisashtonweb@gmail.com>
2017-12-09 12:03:34 +0000

    1.1.0 use permalink rather than title as hook
    Fixes #1

--- avoid-repeating-posts.php
+++ avoid-repeating-posts.php
@@ -2,20 +2,20 @@
 /**
  * Plugin Name: Avoid Repeating Posts
  * Description: Avoid showing same WordPress post in multiple loops. Fresh content in every loop.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: ChrisBAshton
  * Author URI: https://twitter.com/ChrisBAshton
  */
 
- add_filter('the_title', 'arposts__track_displayed_posts');
+ add_filter('post_link', 'arposts__track_displayed_posts');
  add_action('pre_get_posts','arposts__remove_already_displayed_posts');
 
  $displayed_posts = [];
 
- function arposts__track_displayed_posts($title) {
+ function arposts__track_displayed_posts($url) {
      global $displayed_posts;
      $displayed_posts[] = get_the_ID();
-     return $title; // don't mess with the title
+     return $url; // don't mess with the url
  }
 
  function arposts__remove_already_displayed_posts($query) {
